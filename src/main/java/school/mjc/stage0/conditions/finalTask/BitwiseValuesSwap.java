3ab41d4321e901ecf62768fca03c05fe686e2ee6
nstppbl <1nstppbl@gmail.com>
2022-09-03 10:29:50 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first | second;
+        second = first & second;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int res = (dividend/divider) * divider;
+
+        if (divider == 0) {
+            System.out.println("division by zero");
+        } if (res == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println( "cannot be divided completely");
+        }
     }
 }
